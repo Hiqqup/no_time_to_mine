3c6f641f10c2e8f665cdaef33095c69d5c870954
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-04-30 15:20:09 +0200

    implement harvestables placable by tile map
    but i am reconsidering wether thats a terrible idea

--- src/scripts/grid_vector_to_position_converter.gd
+++ src/scripts/grid_vector_to_position_converter.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var grid_vector: Vector2i = Vector2i.ZERO
+
 func grid_vector_to_real_coordinates(grid_vector: Vector2i) -> Vector2:
 	var vector_x = Vector2(grid_vector.x, grid_vector.x)* GlobalConstants.TILE_SIZE ;
 	var vector_y = Vector2(-grid_vector.y, grid_vector.y)* GlobalConstants.TILE_SIZE ;
@@ -7,6 +9,6 @@ func grid_vector_to_real_coordinates(grid_vector: Vector2i) -> Vector2:
 
 
 func set_grid_vector(vector: Vector2i):
-	get_parent().grid_vector = vector;
+	grid_vector = vector;
 	get_parent().global_position =(grid_vector_to_real_coordinates(vector) 
 					+ Vector2(GlobalConstants.TILE_SIZE.x ,0.001));
