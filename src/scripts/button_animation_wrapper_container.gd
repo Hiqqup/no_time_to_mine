a6bf51efcaeb9e7bacbefdef807e9b2483e71d14
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-06-04 18:03:49 +0200

    button animations on new level selector

--- src/scripts/button_animation_wrapper_container.gd
+++ src/scripts/button_animation_wrapper_container.gd
@@ -1,1 +1,22 @@
 extends Control
+
+
+@onready var animation_player: AnimationPlayer = $Wrapper/AnimationPlayer
+@onready var parent: BaseButton = get_parent();
+@onready var wrapper: Control = $Wrapper
+
+func _ready() -> void:
+	
+	wrapper.size = parent.size
+	wrapper.pivot_offset = wrapper.size/2
+	custom_minimum_size = wrapper.size
+	
+	parent.pressed.connect(func():animation_player.play("click"))
+	parent.mouse_entered.connect(func():animation_player.play("hover"))
+	
+	call_deferred("_handle_parenting")
+
+
+func _handle_parenting():
+	reparent(parent.get_parent());
+	parent.reparent(wrapper)
--- src/scripts/new_level_selector.gd
+++ src/scripts/new_level_selector.gd
@@ -2,19 +2,32 @@ extends Control
 
 @onready var level_list: HBoxContainer = $Scale/PanelContainer/MarginContainer/LevelList
 @export var level_types: LevelTypes;
-
+@onready var original_button: TextureButton = $OriginalButton
 
 var _already_displaying: Dictionary[LevelTypes.types, bool];
 var _forge: Forge ; 
 
 func _ready() -> void:
+	var button_in_container = original_button;
+	original_button = original_button.duplicate();
+	button_in_container.queue_free();
+	
 	_forge = get_tree().get_first_node_in_group("forge");
 	for key in LevelTypes.types.size():
 		_already_displaying[key] = false;
 	update();
 
+
+func _init_button():
+	original_button.reparent(level_list);
+	original_button.visible  = true;
+	var ret = original_button.duplicate()
+	original_button.reparent(self);
+	original_button.visible  = false;
+	return ret;
+
 func _generate_button(key: int):
-	var new_button: TextureButton = TextureButton.new();
+	var new_button: TextureButton = _init_button();
 	new_button.pressed.connect(func(): _forge._try_level(key));
 	new_button.texture_normal = level_types.sprite_map[key];
 	level_list.add_child(new_button);
