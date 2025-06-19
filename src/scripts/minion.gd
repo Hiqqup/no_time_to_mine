ea10dd0136d175e5771a7c3836cb52f00d8e494f
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-06-19 20:38:33 +0200

    minion following work in progress

--- src/scripts/minion.gd
+++ src/scripts/minion.gd
@@ -2,11 +2,24 @@ extends CharacterBody2D
 class_name Minion
 
 @export var _level_types: LevelTypes;
+var _player: Player;
 var _forge : Forge
+var speed: float = 50;
 @onready var _sprite_2d: Sprite2D = $Visuals/Sprite2D
+@onready var _navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+
 
 func _physics_process(delta: float) -> void:
 	_forge = get_tree().get_first_node_in_group("forge")
 	_sprite_2d.texture = _level_types.tileset_map[_forge.selected_level]
-	velocity.x = delta *1000;
+	#velocity.x = delta *1000;
+	var dir:= Vector2.ZERO;
+	if position.distance_to(_navigation_agent_2d.target_position)>4.0:
+		dir = to_local(_navigation_agent_2d.get_next_path_position()).normalized()
+	velocity = dir* speed;
+	print(velocity)
 	move_and_slide() 
+
+
+func _on_navigation_update_timer_timeout() -> void:
+	_navigation_agent_2d.target_position  = _player.global_position
--- src/scripts/minion_spawner.gd
+++ src/scripts/minion_spawner.gd
@@ -5,11 +5,11 @@ extends Node2D
 @export var _minion_scene: PackedScene;
 
 func _ready():
-	var y_pos: float = 0;
-	print( _upgrade_stats.minion_amount);
+	var pos_offset: float = 4.0;
 	for i in _upgrade_stats.minion_amount:
 		var minion: Minion = _minion_scene.instantiate();
 		minion.position = _player.position;
-		minion.position.y += y_pos;
-		y_pos+= 15.0;
+		minion.position += Vector2(1,-1) * pos_offset;
+		minion._player = _player;
+		pos_offset+= 4.0;
 		add_child(minion);
