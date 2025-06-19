e75199ca747fd8cb6fc676dc325e9ed2581fc437
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-06-19 21:24:48 +0200

    ditch pathfinding

--- src/scripts/minion.gd
+++ src/scripts/minion.gd
@@ -7,19 +7,35 @@ var _forge : Forge
 var speed: float = 50;
 @onready var _sprite_2d: Sprite2D = $Visuals/Sprite2D
 @onready var _navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
-
+var bounce: Vector2;
+var _walking_time: float 
+@onready var _visuals: Node2D = $Visuals
 
 func _physics_process(delta: float) -> void:
 	_forge = get_tree().get_first_node_in_group("forge")
 	_sprite_2d.texture = _level_types.tileset_map[_forge.selected_level]
-	#velocity.x = delta *1000;
+	
 	var dir:= Vector2.ZERO;
 	if position.distance_to(_navigation_agent_2d.target_position)>4.0:
-		dir = to_local(_navigation_agent_2d.get_next_path_position()).normalized()
+		dir = position.direction_to(_player.global_position);
+		
+	_handle_walking_rotation(dir != Vector2.ZERO, delta)
+	_apply_bounce_handle_dir(dir)
+
+func _apply_bounce_handle_dir(dir:Vector2):
+	dir += bounce;
+	bounce*= 0.93
 	velocity = dir* speed;
-	print(velocity)
 	move_and_slide() 
+	for i in get_slide_collision_count():
+		var collider:Node2D = get_slide_collision(i).get_collider()
+		if collider is StaticBody2D:
+			bounce = collider.global_position.direction_to(global_position) * 2.4;
 
-
-func _on_navigation_update_timer_timeout() -> void:
-	_navigation_agent_2d.target_position  = _player.global_position
+func _handle_walking_rotation(walking: bool, delta: float):
+	if walking:
+		_walking_time += delta;
+		_visuals.rotation = sin(_walking_time * 10)/12;
+	else:
+		_walking_time = 0;
+		_visuals.rotation = lerp(rotation, 0.0, delta * 10);
