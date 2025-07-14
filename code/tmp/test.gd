a6efb1fa06023f2725f5dbb07c5ddce61d445fe0
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-07-14 13:52:40 +0200

    remove tmp folder

--- code/tmp/test.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		$ShockwaveEffect.at(event.position);
